8a0a706bcb58ef80f4e5ad8b89a8753b2d914c63
Jeremy Ben-Meir <jsb459@cornell.edu>
2021-06-12 12:30:48 -0700

    dos cleaning

--- scripts/dos/licenses_src.py
+++ scripts/dos/licenses_src.py
@@ -1,12 +1,94 @@
 from global_vars import *
 
+def clean_addr(row):
+    stopwords = ['FL', 'STORE', 'BSMT','RM','UNIT','STE','APT','APT#','FRNT','#','MEZZANINE','LOBBY','GROUND','FLOOR','SUITE','LEVEL']
+    stopwords1 = ["1ST FLOOR", "1ST FL","2ND FLOOR", "2ND FL","3RD FLOOR", "3RD FL","4TH FLOOR", "4TH FL","5TH FLOOR", "5TH FL","6TH FLOOR", "6TH FL","7TH FLOOR", "7TH FL","8TH FLOOR", "8TH FL","9TH FLOOR", "9TH FL","10TH FLOOR", "10TH FL"]
+    endwords = ["AVE","AVENUE","ST","STREET","ROAD","RD","PLACE","PL","BOULEVARD","BLVD"]
+    
+    # address = row['Address'][0]
+    # zipcode = row['Address'][1]
+
+    if not any(x.isdigit() for x in row['Address'][0]):
+        del row['Address'][0]
+    if "United States" in row['Address'][-1]:
+      del row['Address'][-1]
+
+    if len(row['Address']) > 1:
+      # print(row['Address'][0])
+      row['Address'][0] = row['Address'][0].replace(",","")
+      row['Address'][0] = row['Address'][0].replace(".","")
+      row['Address'][0] = row['Address'][0].replace("\"","")
+      row['Address'][0] = row['Address'][0].replace("-","")
+      row['Address'][0] = row['Address'][0].strip(" ")
+      
+      
+      for word in stopwords1:
+          if word in row['Address'][0].upper():
+              row['Address'][0] = row['Address'][0][:row['Address'][0].upper().index(word)]
+
+      if any(word in row['Address'][0].upper() for word in stopwords):
+          mylist = row['Address'][0].split(" ")
+          for x in range(0, len(mylist)):
+              if mylist[x].upper() in stopwords:
+                  mylist = mylist[:x]
+                  row['Address'][0] = " ".join(mylist)
+                  break
+          
+      mylist = row['Address'][0].split(" ")
+      for x in range(0, len(mylist)):
+          if mylist[x].upper() in endwords:
+              mylist = mylist[:x+1]
+              row['Address'][0] = " ".join(mylist)
+              break
+
+      try:
+          if row['Address'][0].split(" ")[1][:-1].isnumeric() or row['Address'][0].split(" ")[1].isnumeric():
+              mylist = row['Address'][0].split(" ")
+              mylist[1] = mylist[0] + "-" + mylist[1]
+              del mylist[0]
+              row['Address'][0] = " ".join(mylist)
+      except:
+          pass   
+
+      if not " " in row['Address'][0]:
+        row['Building Number']=""
+        row['Street']=""
+      else:
+        row['Building Number'] = row['Address'][0].split(" ")[0]
+        row['Street'] = " ".join(row['Address'][0].split(" ")[1:])
+
+      if not (row['Building Number'].replace("-","").isnumeric()):
+        row['Building Number']=""
+        row['Street']=""
+
+      row['Zip'] = row['Address'][1].split(" ")[-1]
+      
+      if "1/2" in row['Street']:
+        row['Street'] = row['Street'][row['Street'].index("1/2")+3:]
+      
+      if not row['Zip'].isnumeric():
+        if "-" in row["Zip"]:
+          row["Zip"] = row["Zip"].split("-")[0]
+        if row['Address'][-1][-1] == " ":
+          row['Address'][-1] = row['Address'][-1][:-1]
+        row['Zip'] = row['Address'][-1].split(" ")[-1][:5]
+      
+    row['Building Number'] = row['Building Number'].strip(" ")
+    row['Street'] = row['Street'].strip(" ")
+    row['Zip'] = row['Zip'].strip(" ")
+
+    return row
+
+
 def instantiate_file():
   # Get file paths
   aes_file_path = LOCAL_LOCUS_PATH + "data/dos/aes_10-20.csv"
   barber_file_path = LOCAL_LOCUS_PATH + "data/dos/barber_92-20.csv"
+  
   # Get dfs from file paths
   df_aes = pd.read_csv(aes_file_path)
   df_barber = pd.read_csv(barber_file_path)
+
   # Concatenate the dfs  
   df_aes['Industry'] = 'Appearance Enhancement Service'
   df_barber['Industry'] = 'Barber'
@@ -14,31 +96,26 @@ def instantiate_file():
 
   df = df.rename(columns={"License Number": "Record ID", 'Name':'Business Name 2', 'County':'City', 'License State':'State' ,'Phone':'Contact Phone', 'License Issue Date':'LIC Issue Date', 'Current Term Effective Date':'LIC Current Issue Date', 'Expiration Date':'LIC Exp Date', 'License Status':'LIC Status'})
  
-  df['Address'] = df['Address'].apply(lambda x: str(x).replace('                ',' '))
+  df["Record ID"] = df["Record ID"].apply(lambda x : x.replace(" ",""))
+  df["Record ID"] = df["Record ID"].astype(str)
+  df["Business Name 2"] = df["Business Name 2"].astype(str)
 
-#   df['Building Number'] = df['STREET'].apply(lambda x: x.split(" ")[0])
-#   df['Street'] = df['STREET'].apply(lambda x: " ".join(x.split(" ")[1:]))
-#   df['Industry'] =df.apply(lambda row: "FSE-"+str(row['ESTABTYPE1'])+','+str(row['ESTABTYPE2'])+','+str(row['ESTABTYPE3'])+','+str(row['ESTABTYPE4'])+','+str(row['ESTABTYPE5'])+','+str(row['ESTABTYPE6']), axis=1)
+  df["Business Name 2"]=df["Business Name 2"].apply(lambda x:x.replace(u'\xa0', u' '))
+  df["Address"]=df["Address"].apply(lambda x:x.replace(u'\xa0', u' '))
 
-#   df['Business Name 2'] = df['Business Name 2'].astype(str)
-#   df['INSP Date'] = df['INSP Date'].astype('datetime64[D]')
-#   df['LIC Exp Date'] = df['LIC Exp Date'].astype('datetime64[D]')
-#   df['Last INSP Date'] = df['Last INSP Date'].astype('datetime64[D]')
-#   df['# FT Employees'] = df['# FT Employees'].astype(str)
-#   df['# PT Employees'] = df['# PT Employees'].astype(str)
-#   df['# Sq. Ft.'] = df['# Sq. Ft.'].astype(str)
-#   df['Out of Business Date'] = df['Out of Business Date'].astype('datetime64[D]')
+  df["Address"] = df["Address"].apply(lambda x : x.split("                "))
 
-#   del df['Address']
-#   del df['Agency']
-#   del df['ESTABTYPE2']
-#   del df['ESTABTYPE3']
-#   del df['ESTABTYPE4']
-#   del df['ESTABTYPE5']
-#   del df['ESTABTYPE6']
+  df["Building Number"] = ""
+  df["Street"] = ""
+  df["Zip"] = ""
 
-#   df = type_cast(df)
-#   df = clean_zip_city(df)
+  df = df.apply(lambda row : clean_addr(row), axis=1)
+
+  del df["Address"]
+  del df["Agency"]
+
+  df = type_cast(df)
+  # df = clean_zip_city(df)
  
   return df
 
