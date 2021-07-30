7358758465692224dc13fa56f728ee96b97984fe
iggylg1212 <iml2114@columbia.edu>
2021-07-30 17:53:19 -0400

    Changed BBL logic in source_file.py

--- scripts/source_file.py
+++ scripts/source_file.py
@@ -88,7 +88,7 @@ class SourceFile:
         
         if 'BBL' in self.df.columns.to_list():
             self.df['BBL'] = self.df['BBL'].astype(str)
-            self.df['BBL'] = self.df['BBL'].apply(lambda x: '' if math.isnan(float(x)) else ( (str(int(float(x))) if str(int(float(x))).isdigit() else "")))
+            self.df['BBL'] = self.df['BBL'].apply(lambda x: '' if math.isnan(float(x.replace('-','').replace('/',''))) else ( (str(int(float(x.replace('-','').replace('/','')))) if str(int(float(x.replace('-','').replace('/','')))).isdigit() and len(str(int(float(x.replace('-','').replace('/','')))))>9  else "")))
         else:
             self.df['BBL'] = ""
         
