b8b956a5fc4e396a74db504a001e07d6a4be3092
iggylg1212 <iml2114@columbia.edu>
2021-07-31 20:11:10 -0400

    Created industry_assign fn

--- scripts/sentence_trasformers.py
+++ scripts/sentence_trasformers.py
@@ -5,37 +5,32 @@ import csv
 import pickle
 import time
 
-model_name = 'paraphrase-MiniLM-L6-v2'
-model = SentenceTransformer(model_name)
+def industry_assign(inp_list):
+    model_name = 'paraphrase-MiniLM-L6-v2'
+    model = SentenceTransformer(model_name)
 
-max_corpus_size = 100000
-naics = pd.read_csv('/Users/iggy1212 1/Dropbox/locus/data/2017_NAICS_Descriptions.csv')
-naics = naics[naics['Code'].str.len() == 6].reset_index(drop=False)
-corpus_sentences = list(naics['Description'])
+    try:
+        naics = pickle.load(open("/Users/iggy1212 1/Dropbox/locus/data/temp/naics.p", "rb" ))
+        corpus_embeddings = pickle.load(open("/Users/iggy1212 1/Dropbox/locus/data/temp/naics_encodings.p", "rb" ))
 
-print("Encode the corpus. This might take a while")
-corpus_embeddings = model.encode(corpus_sentences, show_progress_bar=True, convert_to_tensor=True)
+    except:
+        naics = pd.read_csv(f'/Users/iggy1212 1/Dropbox/locus/data/2017_NAICS_Descriptions.csv')
+        naics = naics[naics['Code'].str.len() == 6].reset_index(drop=False)
+        corpus_sentences = list(naics['Description'])
+        corpus_embeddings = model.encode(corpus_sentences, show_progress_bar=True, convert_to_tensor=True)
 
-###############################
-print("Corpus loaded with {} sentences / embeddings".format(len(corpus_sentences)))
+        pickle.dump(naics , open("/Users/iggy1212 1/Dropbox/locus/data/temp/naics.p", "wb" ))
+        pickle.dump(corpus_embeddings , open("/Users/iggy1212 1/Dropbox/locus/data/temp/naics_encodings.p", "wb" ))
 
-#Move embeddings to the target device of the model
-corpus_embeddings = corpus_embeddings.to(model._target_device)
-
-while True:
-    inp_question = input("Please enter a question: ")
-
-    start_time = time.time()
-    question_embedding = model.encode(inp_question, convert_to_tensor=True)
+    inp_list = list(set(inp_list))
+    inp = ''
+    for i in inp_list:
+        inp+=f'{i} '
+    
+    question_embedding = model.encode(inp, convert_to_tensor=True)
     hits = util.semantic_search(question_embedding, corpus_embeddings)
-    end_time = time.time()
-    hits = hits[0]  #Get the hits for the first query
+    return naics.iloc[hits[0][0]['corpus_id']]
 
-    print("Input question:", inp_question)
-    print("Results (after {:.3f} seconds):".format(end_time-start_time))
-    for hit in hits[0:5]:
-        print("\t{:.3f}\t{}".format(hit['score'], corpus_sentences[hit['corpus_id']]))
-
-    print("\n\n========\n")
+print(industry_assign(['Cattle farmer','Cattle farmer','Soy farmer','Milk store']))
 
     
