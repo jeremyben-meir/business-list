de5f253fb9444eef442bbef365893babd6f16c51
Jeremy Ben-Meir <jsb459@cornell.edu>
2021-08-29 04:56:44 +0000

    BBL retry

--- scripts/locus.sh
+++ scripts/locus.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 # Run src files 
 #Done : python3 doa/inspection_src.py && python3 doe/pharmacy_src.py && python3 doh/license_src.py && python3 dot/application_src.py && python3 dot/inspection_src.py && python3 dos/license_src.py
-&& python3 doh/inspection_src.py && python3 liq/license_src.py && python3 dca/application_src.py && python3 dca/charge_src.py && python3 dca/inspection_src.py && python3 dca/license_src.py
+# && python3 doh/inspection_src.py && python3 liq/license_src.py && python3 dca/application_src.py && python3 dca/charge_src.py && python3 dca/inspection_src.py && python3 dca/license_src.py
 # Merge
 # python3 record_linkage.py
 # Industry assign
+
+python3 ~/business-list/scripts/doh/inspection_src.py && python3 ~/business-list/scripts/dca/application_src.py && python3 ~/business-list/scripts/dca/inspection_src.py && python3 ~/business-list/scripts/dca/charge_src.py
