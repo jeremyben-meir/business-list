5802a7533a595e5ea3d026a161f11c48b2a064f1
Jeremy Ben-Meir <jsb459@cornell.edu>
2021-08-30 23:41:29 +0000

    show bbl as circles

--- scripts/prepare_geojson.py
+++ scripts/prepare_geojson.py
@@ -4,6 +4,8 @@ from geojson import Feature, FeatureCollection, Point
 from common import DirectoryFields
 import pandas as pd
 import pickle
+import math
+import random
 
 class PrepareGeojson():
 
@@ -14,23 +16,39 @@ class PrepareGeojson():
     def create_llid_json(self):
         df = self.df
         df = df[pd.to_datetime("today") + pd.Timedelta(days=-60) < df["End Date"]]
-        for _, row in df.iterrows():
-            latitude, longitude = map(float, (row["Latitude"], row["Longitude"]))
-            self.features.append(
-                Feature(
-                    geometry = Point((longitude, latitude)),
-                    properties = {
-                        'Name': row["Name"],
-                        'LLID': row["LLID"],
-                        'Address': row["Address"],
-                        'NAICS': str(row["NAICS"]),
-                        'NAICS Title': str(row["NAICS Title"]),
-                        'Contact Phone': str(row["Contact Phone"]),
-                        'Start Date': str(row["Start Date"]),
-                        'End Date': str(row["End Date"])
-                    }
+        for bbl_val in df["BBL"].unique():
+            bbl_df = df[df["BBL"] == bbl_val]
+            num_vals = len(bbl_df)
+            ticker = 0.0
+            lon = bbl_df["Longitude"].max()
+            lat = bbl_df["Latitude"].max()
+            for _,row in bbl_df.iterrows():
+                duration = (row["End Date"] - row["Start Date"]).days
+                duration =  str(math.floor(duration/365.0) if not pd.isnull(duration) else 0)
+                # duration =  ""
+                if num_vals == 1:
+                    latitude, longitude = map(float, (lat, lon))
+                else:
+                    degree_val = 360.0*(ticker/num_vals)
+                    rand_radian = math.radians(degree_val)
+                    latitude, longitude = map(float, (lat+(math.sin(rand_radian)/15000.0), lon+(math.cos(rand_radian)/12000.0)))
+                self.features.append(
+                    Feature(
+                        geometry = Point((longitude, latitude)),
+                        properties = {
+                            'Name': row["Name"],
+                            'LLID': row["LLID"],
+                            'Address': row["Address"],
+                            'NAICS': str(row["NAICS"])[0],
+                            'NAICS Title': str(row["NAICS Title"]),
+                            'Contact Phone': str(row["Contact Phone"]),
+                            "Duration": duration,
+                            'Start Date': str(row["Start Date"]),
+                            'End Date': str(row["End Date"])
+                        }
+                    )
                 )
-            )
+                ticker += 1.0
         collection = FeatureCollection(self.features)
         with open(f'{DirectoryFields.LOCAL_LOCUS_PATH}/data/llid_timeline.json', "w") as f:
             f.write('%s' % collection)
