f047712417012e048fba5660af1b7dd79760037f
Jeremy Ben-Meir <jsb459@cornell.edu>
2022-08-03 05:39:23 +0000

    yearly BBL check in verification

--- scripts/create_verifiable.py
+++ scripts/create_verifiable.py
@@ -2,6 +2,7 @@ import pickle
 from common import DirectoryFields
 import boto3
 import csv
+import pandas as pd
 
 class CreateVerifiable():
 
@@ -22,37 +23,73 @@ class CreateVerifiable():
         pluto_df = pluto_df[["bbl","comarea","unitstotal","unitsres"]]
         return pluto_df
 
-    def convertFile(self):
-        get_path = "data/temp/df-timeline"
-        pluto_df = self.get_pluto(2021)
-        year_list_str = [str(x) for x in list(range(2010,2022))]
+    def get_temp_set(self,year):
+        cur_date = pd.to_datetime(f"01/01/{year}") #TODO edit timestamp
+        next_date = pd.to_datetime(f"01/01/{year+1}") #TODO edit timestamp
+        temp_set = self.df.copy()
+        temp_set = temp_set[(temp_set["Start Date"] >= cur_date) & (temp_set["Start Date"] < next_date)]
+        return temp_set
 
-        print(pluto_df)
+    def convertFile(self):
+        get_path = "data/temp/df-timeline-nosubway"
+        pluto_df = self.get_pluto(2021)
+        year_list = list(range(2010,2022))
+        year_list_str = [str(year) for year in year_list]
+        df = None
 
         self.df = pickle.loads(self.s3.Bucket(DirectoryFields.S3_PATH_NAME).Object(get_path+".p").get()['Body'].read())
-
-        self.df = self.df.merge(pluto_df, how='left', left_on=['BBL'], right_on=['bbl'])
-        self.df = self.df.loc[:,~self.df.columns.duplicated()]
-        self.df = self.df.loc[~self.df.index.duplicated(keep='first')]
-        self.df = self.df.reset_index(drop = True)
-
+        
+        unique_bbl_sample = self.df.sample(n=200)["BBL"].unique()
+        self.df = self.df[self.df["BBL"].isin(unique_bbl_sample)]
+        
         self.df["OSD"] = 1
         self.df["Match?"] = ""
         for col in year_list_str:
             self.df[col] = ""
         self.df["LatLon"] = self.df.apply(lambda row: str(row["Latitude"]) + " " + str(row["Longitude"]),axis=1)
 
-        self.df["comarea"] = self.df["comarea"].fillna(-1).astype(int)
-        self.df["unitstotal"] = self.df["unitstotal"].fillna(-1).astype(int)
-        self.df["unitsres"] = self.df["unitsres"].fillna(-1).astype(int)
-        bad_lbid_mask = (((self.df["unitstotal"] == self.df["unitsres"]) & (self.df["unitstotal"] != -1) & (self.df["unitsres"] != -1)) | (self.df["comarea"] == 0))
-        self.df = self.df[~bad_lbid_mask]
+        for year in year_list:
 
-        unique_bbl_sample = self.df.sample(n=200)["BBL"].unique()
-        self.df = self.df[self.df["BBL"].isin(unique_bbl_sample)]
+            print(year)
 
-        self.df = self.df[['Name', 'Name 1', 'Name 2', 'Name 3', 'OSD', 'Match?', 'BBL', 'Address', 'LatLon', 'Start Date', 'End Date'] + year_list_str]
-        self.df.to_csv(f"{DirectoryFields.S3_PATH}"+get_path+".csv", index=False, quoting=csv.QUOTE_ALL)
+            pluto_df = self.get_pluto(year)
+            temp_set = self.get_temp_set(year)
+
+            merged = temp_set.merge(pluto_df, how='left', left_on=['BBL'], right_on=['bbl'])
+            merged = merged.loc[:,~merged.columns.duplicated()]
+            merged = merged.loc[~merged.index.duplicated(keep='first')]
+            merged = merged.reset_index(drop = True)
+
+            del pluto_df
+            del temp_set
+
+            merged["comarea"] = merged["comarea"].fillna(-1).astype(int)
+            merged["unitstotal"] = merged["unitstotal"].fillna(-1).astype(int)
+            merged["unitsres"] = merged["unitsres"].fillna(-1).astype(int)
+            bad_lbid_mask = (((merged["unitstotal"] == merged["unitsres"]) & (merged["unitstotal"] != -1) & (merged["unitsres"] != -1)) | (merged["comarea"] == 0))
+            merged = merged[~bad_lbid_mask]
+
+            if df is None:
+                df = merged
+            else:
+                df = pd.concat([merged,df],ignore_index=True)
+            
+            print(len(merged))
+            print()
+
+            del merged
+
+        print(df["BBL"].nunique())
+
+        unique_bbl_sample = df["BBL"].unique()[:100]
+        df = df[df["BBL"].isin(unique_bbl_sample)]
+
+        print(df["BBL"].nunique())
+
+        df = df[['Name 1', 'Name 2', 'Name 3', 'OSD', 'Match?', 'BBL', 'Address', 'LatLon', 'Start Date', 'End Date'] + year_list_str]
+        df = df.sort_values(by=['BBL', 'End Date'])
+
+        df.to_csv(f"{DirectoryFields.S3_PATH}"+"data/temp/verifiable.csv", index=False, quoting=csv.QUOTE_ALL)
 
 if __name__ == "__main__":
     creator = CreateVerifiable()
